758f96a91fa932841c95d2ffd63079824c6780ee
Jonathan Velez <jonathan14982@gmail.com>
2023-05-22 12:30:39 -0400

    Bootstrap templates has been installed

--- my_django_app/ROUS/ROUS_API/views.py
+++ my_django_app/ROUS/ROUS_API/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
+from django.shortcuts import render
 
-from ROUS_API.serializers import MaintenanceSerializer, PlaneSerializer, CalenderSerializer, UserSerializer
+from ROUS_API.serializers import (
+    MaintenanceSerializer,
+    PlaneSerializer,
+    CalenderSerializer,
+    UserSerializer,
+)
 from ROUS_API.models import Maintenance_Data, Plane_Data, Calender, User
 
 from django.http import JsonResponse
@@ -8,50 +14,59 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view(['GET', 'POST'])
+def home(request):
+    return render(request, 'home.html')
+
+@api_view(["GET", "POST"])
 def MaintenanceView(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         queryset = Maintenance_Data.objects.all()
         serializerClass = MaintenanceSerializer(queryset, many=True)
         return JsonResponse(serializerClass.data, safe=False)
-    if request.method == 'POST':
+    if request.method == "POST":
         serializerClass = MaintenanceSerializer(data=request.data)
         if serializerClass.is_valid():
             serializerClass.save()
             return Response(serializerClass.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST"])
 def PlaneView(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         queryset = Plane_Data.objects.all()
         serializerClass = PlaneSerializer(queryset, many=True)
         return JsonResponse(serializerClass.data, safe=False)
-    if request.method == 'POST':
+    if request.method == "POST":
         serializerClass = PlaneSerializer(data=request.data)
         if serializerClass.is_valid():
             serializerClass.save()
             return Response(serializerClass.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'POST'])
+
+@api_view(["GET", "POST"])
 def CalenderView(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         queryset = Calender.objects.all()
         serializerClass = CalenderSerializer(queryset, many=True)
         return JsonResponse(serializerClass.data, safe=False)
-    if request.method == 'POST':
+    if request.method == "POST":
         serializerClass = CalenderSerializer(data=request.data)
         if serializerClass.is_valid():
             serializerClass.save()
             return Response(serializerClass.data, status=status.HTTP_201_CREATED)
 
+
 def UserView(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         queryset = User.objects.all()
         serializerClass = UserSerializer(queryset, many=True)
         return JsonResponse(serializerClass.data, safe=False)
-    if request.method == 'POST':
+    if request.method == "POST":
         serializerClass = UserSerializer(data=request.data)
         if serializerClass.is_valid():
             serializerClass.save()
             return Response(serializerClass.data, status=status.HTTP_201_CREATED)
 
+
+def TestView(request):
+    return render(request, {"name": "Bob"})
