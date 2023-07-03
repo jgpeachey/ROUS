989b9e9370ea6f67db6639b38857e1b6e0e21fb5
Jonathan Velez <jonathan14982@gmail.com>
2023-07-03 13:53:10 -0400

    hot fix

--- ROUSProj/ROUSApp/views.py
+++ ROUSProj/ROUSApp/views.py
@@ -8,16 +8,6 @@ from rest_framework import status, generics
 from rest_framework.views import APIView
 
 # views.py
-
-from django.http import JsonResponse
-
-
-def get_license_key(request):
-    license_key = 'GPL-My-Project-Is-Open-Source'
-
-    return JsonResponse({'license_key': license_key})
-
-
 def home(request):
     return render(request, 'home.html')
 
--- ROUSProj/ROUSProj/settings.py
+++ ROUSProj/ROUSProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-emd))y2lt!3q^5zupnhbknc_a)zx6ct2s!8&cwy&=n@(#8isoh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rousapp.com']
+ALLOWED_HOSTS = ['rousapp.com','127.0.0.1']
 
 
 # Application definition
